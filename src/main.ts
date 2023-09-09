ed7981ea3bb47e02c948928fb4f62b7e3af09a41
prd-nguyen-nguyentruong <nguyen.nguyentruong@paradox.ai>
2023-09-09 21:57:22 +0700

    remove unused tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import './index.css'
-
 import App from './App.vue'
 import router from './router'
 
