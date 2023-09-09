7466ca92862d6c8ca1b2b461529a31775c4c314f
prd-nguyen-nguyentruong <nguyen.nguyentruong@paradox.ai>
2023-09-09 19:24:57 +0700

    feature: add common display image card

---
+++ src/components/common/ImageCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import IconCommunity from '../icons/IconCommunity.vue'
+name: 'ImageCard'
+
+const props = defineProps<{anime: object}>()
+</script>
+
+<template>
+    <div>
+        <el-card :class="$style.imageCard">
+            <div :class="$style.hoverText">
+                <h1>{{ props.anime.name }}</h1>
+                <p>ranked</p>
+                <h2>{{ props.anime.rank }}</h2>
+            </div>
+            <img :src="props.anime.imageSource" :class="$style.imageBackground" />
+        </el-card>
+    </div>
+</template>
+
+<style module>
+.imageCard {
+    position: relative;
+    text-align: center;
+
+    .hoverText {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        line-height: 10px;
+
+        > h2 {
+            color: red;
+        }
+    }
+
+    .imageBackground {
+        opacity: 0.3;
+        width: 100%;
+    }
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import NavBar from '../components/common/NavBar.vue'
+import ImageCard from '../components/common/ImageCard.vue'
+
+const anime = {
+    imageSource: 'https://images3.alphacoders.com/132/1322308.jpeg',
+    name: 'Kimi no nawa',
+    rank: 10
+}
 </script>
 
 <template>
     <main>
         <NavBar />
+        <ImageCard :anime="anime"/>
     </main>
 </template>
